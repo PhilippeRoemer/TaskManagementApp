6db961418a32662e6195397cc874821322604111
Philippe Roemer <philipperoemer@yahoo.com>
2022-06-22 13:54:32 -0400

    App cleanup and project outline added

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div>
+            <div className="sidebar">
+                <h1>Projects</h1>
+                <h3>Project 1</h3>
+                <h3>Project 2</h3>
+            </div>
+            <div className="pageContent">
+                <h1>Project Title</h1>
+                <h3>Tasks</h3>
+                <ul>
+                    <li>Task 1</li>
+                    <li>Task 2</li>
+                    <li>Task 3</li>
+                </ul>
+                <h3>Completed Tasks</h3>
+                <ul>
+                    <li>Task 4</li>
+                    <li>Task 5</li>
+                    <li>Task 6</li>
+                </ul>
+            </div>
+        </div>
+    );
 }
 
 export default App;
