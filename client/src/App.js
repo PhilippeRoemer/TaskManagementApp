36e0a5899e45e5dea40ed518709485136376886b
Philippe Roemer <philipperoemer@yahoo.com>
2022-06-23 14:59:54 -0400

    Project task output added

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,64 @@
 import "./App.css";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 function App() {
+    const [listOfProjects, setListOfProjects] = useState([]);
+    const [listOfProjectsTasks, setListOfProjectsTasks] = useState([]);
+
+    const params = {
+        project: { toJSON: () => "Metro App" },
+    };
+
+    useEffect(() => {
+        axios.get("http://localhost:4000/getProject", { params }).then((response) => {
+            console.log(response.data);
+            setListOfProjects(response.data);
+            setListOfProjectsTasks(response.data);
+        });
+    }, []);
     return (
         <div>
             <div className="sidebar">
                 <h1>Projects</h1>
-                <h3>Project 1</h3>
-                <h3>Project 2</h3>
+                {listOfProjects.map((projects) => {
+                    return (
+                        <div>
+                            <h3>Project: {projects.project}</h3>
+                        </div>
+                    );
+                })}
             </div>
             <div className="pageContent">
                 <h1>Project Title</h1>
                 <h3>Tasks</h3>
-                <ul>
-                    <li>Task 1</li>
-                    <li>Task 2</li>
-                    <li>Task 3</li>
-                </ul>
+
+                {listOfProjectsTasks.map((projects) => {
+                    return (
+                        <div className="tasks">
+                            <p>{projects.project}</p>
+                            {projects.project_tasks.map((tasks) => {
+                                if (tasks.completed === true) {
+                                    return <div>{tasks.task}</div>;
+                                }
+                            })}
+                        </div>
+                    );
+                })}
+
                 <h3>Completed Tasks</h3>
-                <ul>
-                    <li>Task 4</li>
-                    <li>Task 5</li>
-                    <li>Task 6</li>
-                </ul>
+                {listOfProjectsTasks.map((projects) => {
+                    return (
+                        <div className="tasks">
+                            <p>{projects.project}</p>
+                            {projects.project_tasks.map((tasks) => {
+                                if (tasks.completed === false) {
+                                    return <div>{tasks.task}</div>;
+                                }
+                            })}
+                        </div>
+                    );
+                })}
             </div>
         </div>
     );
